3f2abc95885e7c69947ab730ef8cb9f801b1af86
github username <github email address>
2020-05-04 11:08:24 -0400

    layout version 1

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,12 @@
 class StaticController < ApplicationController
 
   def home
+    if logged_in? 
+      redirect_to user_path(current_user)
+    else 
+      redirect_to login_path
+    end 
   end
   
+
 end
