8d6bfe4470f46adfea245085274fa23aab2a5720
github username <github email address>
2020-05-04 17:21:31 -0400

    Initial item controller config

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,37 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:edit, :update]
+
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to list_path(@item.list)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to list_path(@item.list)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :due_date, :priority, :assigned_user)
+  end
+
+  def find_item
+    @item = Item.find_by(id: params[:id])
+  end
 end
