6e3ba9a5d98dd1d717ca1c625c7ee7f0f642ccf0
github username <github email address>
2020-05-05 12:13:11 -0400

    Groups version 1

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,50 @@
 class GroupsController < ApplicationController
 
-  def show
-    @group = Group.find_by(id: params[:id])
-  end
+  before_action :found_group, only: [:show, :edit, :update, :delete]
+
+    def index 
+        @groups = Group.all
+    end 
+
+    def show
+      
+    end
+
+    def new
+        @group = Group.new
+        @user = current_user
+    end
+
+    def create
+        @group = Group.new(group_params)
+        if @group.save
+            GroupUser.create(user: current_user, group: @group)
+          redirect_to group_path(@group)
+        else 
+          render :new
+        end
+    end 
+    
+    def edit
+
+    end
+
+    def update
+       if @group.update(group_params)
+           redirect_to group_path(@group)
+       else 
+           render :edit
+       end
+    end
+
+    private
+
+    def found_group
+        @group = Group.find_by(id: params[:id])
+    end
+
+    def group_params
+        params.require(:group).permit(:name)
+    end
 
 end
