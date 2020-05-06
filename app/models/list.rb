5c131a2600369e07d89f617503311107a8d88831
github username <github email address>
2020-05-06 11:09:44 -0400

    Added functionality to add group to new list and existing lists

--- app/models/list.rb
+++ app/models/list.rb
@@ -10,4 +10,15 @@ class List < ApplicationRecord
   def user_usernames
     self.users.map { |u| u.username }.join(", ")
   end
+
+  def add_group(group_id, current_user)
+    group = Group.find_by(id: group_id)
+    if group
+      group.users.each do |user|
+        if !(self.users.include? user)
+          UserList.create(user: user, list: self)
+        end
+      end
+    end
+  end
 end
