55b1b9e3a6487dc94764a0e40bf512b5b9c34265
github username <github email address>
2020-05-08 14:57:47 -0400

    Bug fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,10 +21,10 @@ class User < ApplicationRecord
   end
 
   def sort_lists
-    self.lists.sort_by { |list| list.due_date.nil? ? Time.zone.local(0) : list.due_date }
-    incomplete_lists = self.lists.select { |list| list.items.map { |item| item.complete }.include?(false) }
+    self.lists.sort_by { |list| list.due_date.nil? ? Time.zone.local(99999999) : list.due_date }
+    incomplete_lists = self.lists.select { |list| !list.items.map { |item| item.complete }.include?(false) }
     complete_lists = self.lists.select { |list| list.items.map { |item| item.complete }.all?(true) }
-    incomplete_lists + complete_lists
+    # incomplete_lists + complete_lists
   end
 
   def delete_account
